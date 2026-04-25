9c0a496a9abd087e607880c994b0598f16487b51
EC2 Default User <ec2-user@ip-172-31-16-202.ec2.internal>
2026-04-25 18:16:33 +0000

    Initial commit - Java CI/CD project

---
+++ main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println(greet("Aneela"));
+    }
+
+    public static String greet(String name) {
+        return "Hello, " + name + "!";
+    }
+}
---
+++ test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    public void testGreet() {
+        assertEquals("Hello, Aneela!", App.greet("Aneela"));
+    }
+}
