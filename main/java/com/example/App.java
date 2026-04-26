463029de51a61e5418bf27883156b228edc92b13
veera-ops <106005430+veera-ops@users.noreply.github.com>
2026-04-26 12:40:12 +0530

    Change greeting from 'Aneela' to 'Veera'

--- main/java/com/example/App.java
+++ main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(greet("Aneela"));
+        System.out.println(greet("Veera"));
     }
 
     public static String greet(String name) {
